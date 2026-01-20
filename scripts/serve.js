4248300377a0e5ef450cd0cd0981871172414b97
Steve Shreeve <steve.shreeve@gmail.com>
2026-01-19 22:13:47 -0700

    Serve *.txt and *.md in browser (not as downloads)

--- scripts/serve.js
+++ scripts/serve.js
@@ -18,6 +18,8 @@ const MIME_TYPES = {
   '.js': 'application/javascript; charset=utf-8',
   '.css': 'text/css; charset=utf-8',
   '.json': 'application/json',
+  '.txt': 'text/plain; charset=utf-8',
+  '.md': 'text/plain; charset=utf-8',
   '.png': 'image/png',
   '.jpg': 'image/jpeg',
   '.svg': 'image/svg+xml',
