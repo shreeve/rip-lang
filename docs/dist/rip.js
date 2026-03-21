06f9f05b2f86c02035d99121c7f97c35addab1b3
Steve Shreeve <steve.shreeve@gmail.com>
2026-03-21 01:22:08 -0600

    Release rip-lang 3.13.128
      @rip-lang/ui@0.4.8
      @rip-lang/swarm@1.2.117
      @rip-lang/x12@0.2.107
      @rip-lang/server@1.3.123
      @rip-lang/print@1.1.111
      @rip-lang/script@0.1.3
      @rip-lang/schema@0.3.107
      @rip-lang/http@1.1.107
      @rip-lang/csv@1.3.106
      @rip-lang/db@1.3.116
      @rip-lang/stamp@0.1.27
      @rip-lang/all@3.13.154

--- docs/dist/rip.js
+++ docs/dist/rip.js
@@ -9999,8 +9999,8 @@ globalThis.zip    ??= (...a) => a[0].map((_, i) => a.map(b => b[i]));
     return new CodeGenerator({}).getComponentRuntime();
   }
   // src/browser.js
-  var VERSION = "3.13.127";
-  var BUILD_DATE = "2026-03-21@07:18:26GMT";
+  var VERSION = "3.13.128";
+  var BUILD_DATE = "2026-03-21@07:22:07GMT";
   if (typeof globalThis !== "undefined") {
     if (!globalThis.__rip)
       new Function(getReactiveRuntime())();
